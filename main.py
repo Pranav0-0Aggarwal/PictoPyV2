2f1fb62921e1e9e515b3659adae817b49b7e6355
ycantistudy <ycantistudy@gmail.com>
2024-06-03 14:49:19 +0000

    initial comments for main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+# Check if DB file exists
+# If it doesn't create it
+# check if it contain all classes
+# if not add them according to their indexs
+# if conflit occurs delete DB and recreate
+
+
+# periodically run the object detection function and compare it with DB
