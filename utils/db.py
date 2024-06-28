35e32ca2de9a3e18b7941b562072bd6b134f1467
ycantistudy <ycantistudy@gmail.com>
2024-06-28 20:04:19 +0000

    added groupByClass() and layed foundation for basic gallery features

--- utils/db.py
+++ utils/db.py
@@ -64,6 +64,27 @@ def hashExist(conn: sqlite3.Connection, hashValue: str) -> bool:
     result = executeQuery(conn, query)
     return result[0][0] == 1
 
+
+def groupByclasses(conn: sqlite3.Connection) -> List[Tuple[str, str]]:
+    """Returns hashes grouped by classes from the database.
+
+    Args:
+        conn: A sqlite3.Connection object.
+
+    Returns:
+        list: A list of tuples where each tuple contains class name and concatenated hashes.
+        list[0][0]: class name
+        list[0][1]: concatenated paths
+    """
+    query = """
+        SELECT c.class, GROUP_CONCAT(i.path)
+        FROM CLASS c
+        JOIN JUNCTION j ON c.classID = j.classID
+        JOIN IMAGES i ON j.imageID = i.imageID
+        GROUP BY c.class
+    """
+    return executeQuery(conn, query)
+
 def toggleVisibility(conn: sqlite3.Connection, paths: List[str], hidden: int) -> None:
     """Switch visibility of images by changing value of hidden column.
 
@@ -74,3 +95,37 @@ def toggleVisibility(conn: sqlite3.Connection, paths: List[str], hidden: int) ->
     """
     query = f"UPDATE media SET hidden={hidden} WHERE path IN ({', '.join(['?'] * len(paths))})"
     executeQuery(conn, query, paths)
+
+def hideByClass(conn: sqlite3.Connection, classes: List[str]) -> None:
+    """Hides images by class.
+
+    Args:
+        conn: sqlite3.Connection object.
+        classes: A list of class names.
+    """
+
+def unhideByClass(conn: sqlite3.Connection, classes: List[str]) -> None:
+    """Unhides images by class.
+
+    Args:
+        conn: sqlite3.Connection object.
+        classes: A list of class names.
+    """
+
+def delete(conn: sqlite3.Connection, paths: List[str]) -> None:
+    """Prompt for confirmation.
+    Deletes images by path.
+    Deletes entry from DB considering junction table.
+
+    Args:
+        conn: sqlite3.Connection object.
+        paths: A list of paths to delete.
+    """
+
+def deleteByClass(conn: sqlite3.Connection, classes: List[str]) -> None:
+    """Deletes images by class.
+
+    Args:
+        conn: sqlite3.Connection object.
+        classes: A list of class names.
+    """
